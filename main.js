61e9983853cdf00296e82b186b81845bf689ad47
Tajamul Wani <thetj4054@gmail.com>
2024-12-17 13:04:20 +0530

    Removed testimonials and changed UI of social icons

--- main.js
+++ main.js
@@ -6,15 +6,6 @@ MenuIcon.addEventListener('click', () => {
     navbar.classList.toggle('active');
 })
 z
-/* Swiper Js*/
-
-const swiper = new Swiper('.swiper',{
-  effect: 'slide',
-  loop: true,
-  autoplay : {
-    delay : 2500,
-  },
-})
 
 
 
